09606f76f994b0a75bb778f5e9dd97a31a51f892
rafalmiler77 <rafalmiler77@gmail.com>
2017-05-24 20:54:07 +0200

    simple form added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  onInputChange = e => {
+    console.log('click')
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Auto-completion</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Input a github user login:
         </p>
+        <div className="gh-form">
+          <input
+            type="text"
+            name="name here"
+            onChange={e => this.onInputChange(e)}
+            className="gh-input"
+            value='some value'
+          />
+        </div>          
       </div>
     );
   }
