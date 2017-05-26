ff019370e2290e856f7d462cc2a4c5dee0ea758a
rafalmiler77 <rafalmiler77@gmail.com>
2017-05-26 17:05:04 +0200

    autocompletion logic works

--- src/input-form/InputForm.js
+++ src/input-form/InputForm.js
@@ -29,25 +29,86 @@ class InputForm extends Component {
     this.setState({
       inputValue: e.target.value
     })
-    this.props.getUser(e.target.value)
+    // this.props.getUser(e.target.value)
+      // this.prepareDisplayedItems()
+  }
+  
+  prepareDisplayedItems = () => {
+    const people = this.props.people;
+    console.log('people', this.props.people)
+
+    // This regex filters items depending on input value
+    let pattern = new RegExp("\\b" + this.state.inputValue);
+    const regexedPeople = people.filter(
+      man => pattern.test(man.name),
+    );
+
+    console.log('regexedPeople', regexedPeople)
+    // It limits displayed number of items 
+    const displayLimiter = 8;
+    const mappedPeople = regexedPeople.slice(0, displayLimiter).map(
+      man => <p key={man.id}>{man.name}</p>
+    );
+    console.log('mappedPeople', mappedPeople)
+
+    // this.setState({ availablePeople: mappedPeople });
+    // return mappedPeople
   }
 
   render() {
-    console.log('this.props.users', this.props.users)
+    const people = this.props.people;
+    console.log('people', this.props.people)
+
+    // This regex filters items depending on input value
+    let pattern = new RegExp("\\b" + this.state.inputValue);
+    const regexedPeople = people.filter(
+      // item => item.name === this.state.inputValue,
+      item => pattern.test(item.name)
+
+    );
+
+    console.log('regexedPeople', regexedPeople)
+    // It limits displayed number of items 
+    const displayLimiter = 8;
+    const mappedPeople = regexedPeople.slice(0, displayLimiter).map(
+      man => <p key={man.id}>{man.name}</p>
+    );
+    console.log('mappedPeople', mappedPeople)
+    console.log('this.props.people[0]', this.props.people[0])
+    console.log('this.props.people[0].name', this.props.people[0])
     return (
       <div className="gh-form">
-        <label>Input a github user login:</label>
+        {/*<label>Input a github user login:</label>
         <br />
         <input
           type="text"
-          name="name here"
+          name="ghLogin"
+          onChange={e => this.onInputChange(e)}
+          className="gh-input"
+          value={this.state.inputValue}
+        />*/}
+        <label>Input a name from mock list:</label>
+        <br />
+        <input
+          type="text"
+          name="mockName"
           onChange={e => this.onInputChange(e)}
           className="gh-input"
           value={this.state.inputValue}
         />
+        <div>
+
+          {
+            mappedPeople
+          }
+        </div>
       </div>
     );
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(InputForm);
+
+  {/*this.state.availablePeople*/ }
+  {/*{this.prepareDisplayedItems}*/ }
+
